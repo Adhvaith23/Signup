7d033a980589932d8801bac2e40f85fcfc361d1c
adhvaith <adhvaithpn10@gmail.com>
2022-07-14 10:48:08 +0530

    Add project files.

---
+++ Loginmvc/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Loginmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loginmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Login obj)
+        {
+            if (ModelState.IsValid)
+            {
+                registrationEntities db = new registrationEntities();
+                db.Logins.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
